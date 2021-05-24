7a48b16f70e9c0a9ad8a812227cd094137dadd75
thisisjaskaran <sodhi.jaskaransingh@gmail.com>
2021-05-24 10:40:57 +0530

    double pointers and memset

--- pointers.cpp
+++ pointers.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
@@ -7,5 +8,13 @@ int main()
     int var = 8;
     void *ptr = &var;
     cout << ptr << endl;
+
+    char *buffer = new char[8];
+    memset(buffer, 0, 8);
+
+    char **double_ptr = &buffer;
+
+    delete[] buffer;
+
     return 0;
 }
